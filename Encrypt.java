9e09c7aa5d2e38cbb2156f14f4c64e94f7aa0727
Brian <70177580+slept668@users.noreply.github.com>
2025-03-12 20:52:15 -0400

    Update Encrypt.java
    Decoder works both ways now

--- Encrypt.java
+++ Encrypt.java
@@ -1,13 +1,26 @@
 package cop2805;
 
 public class Encrypt {
-	public static String Encrypter(String stringy) {
+	public static String Decrypter(String stringy) {
 		int i;
 		String coded = "";
 		char index;
 		
 		for (i = 0; i < stringy.length(); i++) {
 			index = (char) (stringy.charAt(i) + 10);
+			coded += index;
+		}
+		
+		return coded;
+	}
+	
+	public static String Encrypter(String stringy) {
+		int i;
+		String coded = "";
+		char index;
+		
+		for (i = 0; i < stringy.length(); i++) {
+			index = (char) (stringy.charAt(i) - 10);
 			coded += index;
 		}
 		
